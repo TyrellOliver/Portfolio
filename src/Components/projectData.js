8f9025c6a2a5068839b5125e4e5459e724ec2a69
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-09-05 10:12:36 -0400

    feat: added initial project data to portfolio

---
+++ src/Components/projectData.js
@@ -0,0 +1,60 @@
+const projects = [
+  {
+    id: 1,
+    title: "GoalHive",
+    description:
+      "GoalHive is a community-based application where members team up to support, motivate, and hold each other accountable while completing interest-related goals and/or projects.",
+    image: "",
+    techStack: ["PostgreSQL", "Express.js", "React", "Node,js"],
+    links: [
+      { label: "Live Demo", url: "" },
+      {
+        label: "GitHub",
+        url: "https://github.com/TyrellOliver/Capstone-FrontEnd",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Mooyoshi",
+    description:
+      "Mooyoshi: An interactive map app dedicated to helping Japanese culture enthusiasts explore authentic locations, savor traditional cuisine, and stay updated on Japanese events in their city.",
+    image: "",
+    techStack: [],
+    links: [
+      { label: "Live Demo", url: "" },
+      {
+        label: "GitHub",
+        url: "",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Youtube Clone",
+    description: "",
+    image: "",
+    techStack: [],
+    links: [
+      { label: "Live Demo", url: "" },
+      {
+        label: "GitHub",
+        url: "",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Game",
+    description: "",
+    image: "",
+    techStack: [],
+    links: [
+      { label: "Live Demo", url: "" },
+      {
+        label: "GitHub",
+        url: "",
+      },
+    ],
+  },
+];
